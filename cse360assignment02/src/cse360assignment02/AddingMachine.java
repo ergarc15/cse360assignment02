360c834898bc8825a65ef43b828ebd67249ffda9
ericgarcia <ericgarcia@192.168.0.90>
2020-10-06 15:14:58 -0700

    Initial commit of project

---
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,57 @@
+package cse360assignment02;
+
+/**
+ * This class executes a few simple methods
+ * @author ericgarcia
+ * @version 1.0
+ */
+
+public class AddingMachine {
+	private int total;
+	  
+	
+	
+	  public AddingMachine () {
+	    total = 0;  // not needed - included for clarity
+	  }
+	  
+	  /**
+	   * This returns the interger total
+	   * @return is the current total
+	   */
+	  
+	  public int getTotal () {
+	    return 0;
+	  }
+	  
+	  /**
+	   * This adds the parameter to the value 
+	   * @param value
+	   */
+	  
+	  public void add (int value) {
+	  }
+
+	  /**
+	   * This subtracts the parameter to the value
+	   * @param value
+	   */
+	  
+	  public void subtract (int value) {
+	  }
+
+	  /**
+	   * 
+	   */
+	  
+	  public String toString () {
+	    return "";
+	  }
+	  
+	  /**
+	   * This clears the memory
+	   */
+
+	  public void clear() {
+	  }
+}
